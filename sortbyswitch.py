00d7d69ec05d053166e0744855b081b594adeb81
Jeanne Chris <69834505+Jeanne-Chris@users.noreply.github.com>
2021-01-08 18:15:31 +0800

    Update sortbyswitch.py

--- sortbyswitch.py
+++ sortbyswitch.py
@@ -3,8 +3,7 @@ numList = [7894, 4304, 9084, 0, 7844, 9893, 1545, 2000, 8943, 42, 332, 9, 10]
 numList.sort()
 print("numList: ", numList)
 
-# It is also possible to sort a list through a boolean switch and iteration testing of values
-# and return the same result.
+# It is also possible to sort a list through a boolean switch and iteration testing of values and return the same result.
 newLst = [7894, 4304, 9084, 0, 7844, 9893, 1545, 2000, 8943, 42, 332, 9, 10]
 switch = True
 
